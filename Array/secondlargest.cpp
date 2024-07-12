bc4206dbb1e1fcb756cb15f765fcfce9578ef420
Deepak kumar <deepaks9631342845@gmail.com>
2024-07-12 23:03:29 +0530

    day1

---
+++ Array/secondlargest.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int print2largest(vector<int> &arr) {
+        // Code Here
+        int largest=arr[0];
+        int secondlargest=-1;
+        for(int i=1; i<arr.size(); i++){
+            if(arr[i]>largest){
+                secondlargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>secondlargest){
+                secondlargest=arr[i];
+            }
+        }
+        return secondlargest;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.print2largest(arr);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
