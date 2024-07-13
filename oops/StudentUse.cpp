3e6a471abc5d4d7685d0554eb243000a624daa1f
Deepak kumar <deepaks9631342845@gmail.com>
2024-07-14 01:14:03 +0530

    oops2

--- oops/StudentUse.cpp
+++ oops/StudentUse.cpp
@@ -37,3 +37,24 @@ int main() {
 	
 }
 
+
+static function
+
+#include <iostream>
+using namespace std;
+#include "Student.cpp"
+
+int main() {
+	Student s1;
+
+	Student s2;
+
+	Student s3, s4, s5;
+
+	// cout << Student :: totalStudents << endl;
+
+	cout << Student :: getTotalStudent() << endl;
+
+}
+
+
